c3a8bfbe8ea3481fb192f7f83136b0c45ba1443f
Boussive21 <99147715+Boussive21@users.noreply.github.com>
2022-07-03 19:40:53 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+#DB_PASSWORD
+db_password = 'Nut#0420'
+db_string = f"postgresql://postgres:{db_password}@127.0.0.1:5432/movie_data"
+
+#API Keys
+api_key = 'f02c728741e51d16681562e500eefb08'
+g_key = 'AIzaSyChLbp8teatz_8Pf4avDKUItR6Q354629E'
